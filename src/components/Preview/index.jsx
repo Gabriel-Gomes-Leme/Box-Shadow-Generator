e93473f79419354e7240e459f1bb3cbce96a4540
Gabriel Gomes <gabrielgl20@outlook.com>
2023-01-05 21:09:37 -0300

    Fixed: box preview

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -10,11 +10,11 @@ export const Preview = ({data}) => {
         <div className="form-control">
           <input type="color" name="color" id="color" value={color} onChange={(e)=>setColor(e.target.value)}/>
         </div>
-        <div className="box-effect" style={{background:color, boxShadow:boxShadow}}></div>
+          <div className="box-effect" style={{background:color, boxShadow:boxShadow}}></div>
         <div className="rules">
           <h3>Copie o código abaixo e cole no seu arquivo CSS:</h3>
           <div className="rule">
-            <p><span className="primary-color">box-shadow:</span> {boxShadow}</p>
+            <p><span className="primary-color">box-shadow:</span> {boxShadow};</p>
           </div>
         </div>
     </div>
